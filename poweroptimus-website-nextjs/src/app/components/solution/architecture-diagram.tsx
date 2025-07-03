7043c93391cc59069cf5550031b1344ce7f137b3
Chinmaywani28 <103192458+Chinmaywani28@users.noreply.github.com>
2025-07-03 19:24:42 +0530

    solution architecture section

--- poweroptimus-website-nextjs/src/app/components/solution/architecture-diagram.tsx
+++ poweroptimus-website-nextjs/src/app/components/solution/architecture-diagram.tsx
@@ -4,12 +4,17 @@ import './architecture-diagram.css'; // Import HomePage specific styles
 
 const ArchitectureDiagram = () => {
   return (
-     <section className="architecture-section">
-      <img
-        src="./system-architecture.png"
-        alt="System Architecture Diagram"
-        className="architecture-image"
-      />
+    //  <section className="architecture-section">
+    //   <img
+    //     src="./system-architecture.png"
+    //     alt="System Architecture Diagram"
+    //     className="architecture-image"
+    //   />
+    // </section>
+
+    <section className="process-section">
+      {/* <h2 className="process-title Header2">Architecture-section</h2> */}
+      <div className="diagram-placeholder Header2">&lt; System Architecture Diagram &gt;</div>
     </section>
   )
 }
