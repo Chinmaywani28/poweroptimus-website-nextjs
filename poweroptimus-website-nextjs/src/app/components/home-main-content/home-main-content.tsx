d2d10e865d31ae0abe9dc8f165fe3383bc975825
Chinmaywani28 <103192458+Chinmaywani28@users.noreply.github.com>
2025-07-08 11:50:21 +0530

    error

--- poweroptimus-website-nextjs/src/app/components/home-main-content/home-main-content.tsx
+++ poweroptimus-website-nextjs/src/app/components/home-main-content/home-main-content.tsx
@@ -3,13 +3,14 @@ import './home-main-content.css'
 import Hero from './hero';
 import { useTranslation } from 'react-i18next';
 import Slider from '../slider/slider';
-import WhyEnergy from '../whyEnergy/WhyEnergy';
+// import WhyEnergy from '../whyEnergy/WhyEnergy';
 import Homehowitwork from '@/app/homehowitwork/homehowitwork';
 
 import Homefeature from '../homefeature/homefeature';
 import Keyvertical from '../keyvertical/keyvertical';
 import Table from '../table1/table';
 import TableTwo from '../table2/tabletwo';
+import WhyEnergy from '../whyEnergy/whyenergy';
 
 
 
