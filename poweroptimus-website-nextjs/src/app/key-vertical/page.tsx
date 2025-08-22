a3996a527bbf386d36c9f81ea1f71e635d947cd4
Chinmaywani28 <chinmaywani77@gmail.com>
2025-08-22 12:21:17 +0530

    amend

--- poweroptimus-website-nextjs/src/app/key-vertical/page.tsx
+++ poweroptimus-website-nextjs/src/app/key-vertical/page.tsx
@@ -90,54 +90,7 @@ const page = () => {
         showList={false}
       />
 
-      {/* keyvertical two */}
-      {/* <KeyverticalTwo
-        title="Smart Device Compatibility"
-        description="Explore the wide range of devices and sensors we support for precise energy monitoring and management. From HVAC systems and temperature sensors to air quality monitors and beyond, our platform integrates with a variety of devices to provide you with comprehensive insights into your energy consumption and environmental conditions."
-        subLink='All Industries'
-      /> */}
-
-      {/* hvac sections */}
-      {/* <KeyverticalTwo
-        title="Smart Device Compatibility"
-        description="Explore the wide range of devices and sensors we support for precise energy monitoring and management. From HVAC systems and temperature sensors to air quality monitors and beyond, our platform integrates with a variety of devices to provide you with comprehensive insights into your energy consumption and environmental conditions."
-        showLink={true}
-        linkHref="/industries"
-        linkText="All Industries"
-        iconText="←"
-      />
-
-      <FacilitySectionsTwo
-        title="Quis totam voluptatem maiores at explicabo architecto sed voluptatem laudantium"
-        subtitle="HVAC Controls and Monitoring"
-        description="Vitae incidunt omnis suscipit omnis natus non. Facilis non omnis id iure qui quaerat voluptate aliquam. Quis totam voluptatem maiores at explicabo architecto sed voluptatem laudantium. Vitae similique totam. chine operations, reducing downtime, and minimizing waste.PowerOptimus improves energy efficiency in production facilities by analyzing energy consumption against committed demand thus optimizing machine operations, reducing downtime"
-        imageSrc="./key-vertical-sec-one.png"
-        imageLeft={false}
-      />
-
-      <FacilitySectionsTwo
-        title="Quis totam voluptatem maiores at explicabo architecto sed voluptatem laudantium"
-        subtitle="AQI Monitoring"
-        description="Vitae incidunt omnis suscipit omnis natus non. Facilis non omnis id iure qui quaerat voluptate aliquam. Quis totam voluptatem maiores at explicabo architecto sed voluptatem laudantium. Vitae similique totam. chine operations, reducing downtime, and minimizing waste.PowerOptimus improves energy efficiency in production facilities by analyzing energy consumption against committed demand thus optimizing machine operations, reducing downtime"
-        imageSrc="./key-vertical-sec-two.png"
-        imageLeft={true}
-      />
-
-      <FacilitySectionsTwo
-        title="Quis totam voluptatem maiores at explicabo architecto sed voluptatem laudantium "
-        subtitle="Temperature Monitoring"
-        description="Vitae incidunt omnis suscipit omnis natus non. Facilis non omnis id iure qui quaerat voluptate aliquam. Quis totam voluptatem maiores at explicabo architecto sed voluptatem laudantium. Vitae similique totam. chine operations, reducing downtime, and minimizing waste.PowerOptimus improves energy efficiency in production facilities by analyzing energy consumption against committed demand thus optimizing machine operations, reducing downtime"
-        imageSrc="./key-vertical-sec-three.png"
-        imageLeft={false}
-      />
-
-      <FacilitySectionsTwo
-        title="Quis totam voluptatem maiores at explicabo architecto sed voluptatem laudantium"
-        subtitle="Early Fault Detection "
-        description="Vitae incidunt omnis suscipit omnis natus non. Facilis non omnis id iure qui quaerat voluptate aliquam. Quis totam voluptatem maiores at explicabo architecto sed voluptatem laudantium. Vitae similique totam. chine operations, reducing downtime, and minimizing waste.PowerOptimus improves energy efficiency in production facilities by analyzing energy consumption against committed demand thus optimizing machine operations, reducing downtime"
-        imageSrc="./key-vertical-sec-four.png"
-        imageLeft={true}
-      /> */}
+      
 
       {/* <WhyPowerOptimus/> */}
       <WhyPowerOptimusTwo/>
