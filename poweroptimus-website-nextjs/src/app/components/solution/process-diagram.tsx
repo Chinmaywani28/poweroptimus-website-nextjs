cd1e1c9570f9d1ad6ea9c830814478747c557050
Chinmaywani28 <chinmaywani77@gmail.com>
2025-09-23 19:11:19 +0530

    process dia change

--- poweroptimus-website-nextjs/src/app/components/solution/process-diagram.tsx
+++ poweroptimus-website-nextjs/src/app/components/solution/process-diagram.tsx
@@ -5,7 +5,7 @@ import './process-diagram.css'; // Import HomePage specific styles
 const ProcessDiagram = () => {
   return (
     <section className="process-section">
-      <h2 className="Header1sec marTop">Process</h2>
+      {/* <h2 className="Header1sec marTop">Process</h2> */}
       <div className="diagram-placeholder Header1sec marTop">
 
         <img src="/process-diagram-three.jpg" alt="System Architecture" />
