728a16b1d528490eebdf476f87f856d9309ca47d
Chinmaywani28 <chinmaywani77@gmail.com>
2025-11-19 18:37:06 +0530

    firebase changes

--- poweroptimus-website-nextjs/next.config.ts
+++ poweroptimus-website-nextjs/next.config.ts
@@ -1,9 +1,23 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  // output: "export", // ✅ Enables static export
+// const nextConfig: NextConfig = {
+//   // output: "export", // ✅ Enables static export
+//   images: {
+//     unoptimized: true, // ✅ Needed for static export with <Image />
+//   },
+// };
+
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",              // ⭐ REQUIRED for static hosting
+  trailingSlash: true,           // ⭐ Firebase Hosting needs trailing slash
   images: {
-    unoptimized: true, // ✅ Needed for static export with <Image />
+    unoptimized: true,           // REQUIRED for export when using <Image />
+  },
+  experimental: {
+    typedRoutes: false,
   },
 };
 
