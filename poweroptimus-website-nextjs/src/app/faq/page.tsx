7978a43e53199e9d8fcd78b6d9d71287deaa7a7b
Chinmaywani28 <chinmaywani77@gmail.com>
2026-06-16 15:23:00 +0530

    amend

--- poweroptimus-website-nextjs/src/app/faq/page.tsx
+++ poweroptimus-website-nextjs/src/app/faq/page.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next';
 import './page.css'
+import FAQComponent from '../components/solution/faq-component';
 
 const Faq = () => {
   const { t } = useTranslation(); // Initialize translation hook
@@ -10,7 +11,7 @@ const Faq = () => {
     <>
    
     {/* <div>coming soon</div> */}
-
+      <FAQComponent/>
     </>
   );
 }
